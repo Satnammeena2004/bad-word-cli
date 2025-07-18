cbff741a83835137f6d3468aef072653928ab88a
Satnam Meena <satnam.meena2004@gmail.com>
2025-07-18 15:13:09 +0530

    version changes

--- bin/index.js
+++ bin/index.js
@@ -11,7 +11,7 @@ const program = new Command();
 
 program
   .name("bad-word-cli")
-  .version("0.2.0")
+  .version("1.2.0")
   .description(
     "A CLI tool to detect and clean profane words using the 'bad-words' package."
   );
